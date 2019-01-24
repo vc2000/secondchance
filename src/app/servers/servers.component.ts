147372f04350a513df6f674651aa89d93bad9467
01653187 <01653187@corp.root>
2019-01-24 14:58:39 +0800

    ngif and hidden functions

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No Server was created!';
   serverName = 'Testserver';
+  serverCreated = false;
 
   constructor() {
   setTimeout(() => { this.allowNewServer = true;} ,2000);
@@ -18,6 +19,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created!';
   }
   onUpdateServerName(event: Event){
