2666153bbc43a84d2cc9222d8208c77f6ac6d92a
01653187 <01653187@corp.root>
2019-01-25 16:32:38 +0800

    fixed bug

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipe: Recipe[]= [
+    new Recipe('choco','taste good','https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chocolate-fudge-cake.jpg'),
+    // new Recipe('chicken','yummy','https://www.thegunnysack.com/wp-content/uploads/2018/03/Honey-Garlic-Chicken-Recipe.jpg')
+  ];
 
   constructor() { }
 
